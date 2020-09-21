700f52fd81f563bd3606a6e77c28d619830c4fe1
Yalin Yarimagan <yalin.yarimagan@gmail.com>
2020-09-21 19:25:02 +0300

    end of lesson 44

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,21 @@
 const express = require('express')
+const forecast = require('../../weather-app/utils/forecast')
 
 const app = express()
 
 app.get('', (req, res) => res.send("hello"))
 app.get('/help', (req, res) => res.send("this is help page"))
 app.get('/about', (req, res) => res.send("<html><body><h1>this is about</body></html>"))
-app.get('/weather', (req, res) => res.send("bir siktir git"))
+app.get('/weather', (req, res) => {
+    
+    forecast(42, 29, (forecastError, forecastData) => {
+        if (forecastError) {
+            return res.send(forecastError)
+        }        
+        res.send(forecastData)
+    })  
+     
+})
 app.get('*', (req, res) => res.send(`bir siktir git ${req.path}`))
 
 
