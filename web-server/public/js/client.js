ebc0e2030627bd4456e6666cafcf5b69bb807d3d
Yalin Yarimagan <yalin.yarimagan@gmail.com>
2020-09-22 16:50:21 +0300

    end of section 8

--- web-server/public/js/client.js
+++ web-server/public/js/client.js
@@ -3,8 +3,14 @@ console.log("hello amk")
 const weatherForm = document.querySelector('form')
 const addressTxt = document.querySelector('input')
 
+const line1 = document.querySelector('#line1')
+const line2 = document.querySelector('#line2')
+
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
+
+    line1.textContent = "loading.."
+    line2.textContent = ""
 
     const location = addressTxt.value
     console.log(`location= ${location}`)
@@ -12,11 +18,13 @@ weatherForm.addEventListener('submit', (e) => {
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error)
-                console.log(data.error)
-            else {
-                //console.log(data)
-                console.log(`place: ${data.place}`)
-                console.log(`temp: ${data.temp}`)
+                //console.log(data.error)
+                line1.textContent = data.error
+            else {                
+                //console.log(`place: ${data.place}`)
+                //console.log(`temp: ${data.temp}`)
+                line1.textContent = `place: ${data.place}`
+                line2.textContent = `temp: ${data.temp}`
             }
         })
     })
