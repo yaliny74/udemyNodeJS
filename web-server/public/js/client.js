06906c46942be643e77b017890ea6d0b602933f2
Yalin Yarimagan <yalin.yarimagan@gmail.com>
2020-09-22 15:43:21 +0300

    end of lesson58

--- web-server/public/js/client.js
+++ web-server/public/js/client.js
@@ -1,13 +1,23 @@
 console.log("hello amk")
 
-fetch('http://localhost:3000/weather?address=dublin').then((response) => {
-    response.json().then((data) => {
-        if (data.error)
-            console.log(data.error)
-        else {
-            console.log(data)
-            console.log(`place: ${data.place}`)
-            console.log(`temp: ${data.temp}`)
-        }
+const weatherForm = document.querySelector('form')
+const addressTxt = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = addressTxt.value
+    console.log(`location= ${location}`)
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error)
+                console.log(data.error)
+            else {
+                //console.log(data)
+                console.log(`place: ${data.place}`)
+                console.log(`temp: ${data.temp}`)
+            }
+        })
     })
 })
