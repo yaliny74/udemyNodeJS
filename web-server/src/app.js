bfe5aa1034d9497cbf76f1817203689d8f562ff0
Yalin Yarimagan <yalin.yarimagan@gmail.com>
2020-09-22 10:00:18 +0300

    ed of lesson 47

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,10 +1,21 @@
+const path = require('path')
 const express = require('express')
+
 const forecast = require('../../weather-app/utils/forecast')
+const { networkInterfaces } = require('os')
+const pubDir = path.join(__dirname, "../public")
 
 const app = express()
 
-app.get('', (req, res) => res.send("hello"))
-app.get('/help', (req, res) => res.send("this is help page"))
+app.set('view engine', 'hbs')
+app.use(express.static(pubDir))
+
+app.get('/help', (req, res) => {
+    res.render('help.hbs', {
+        'msg': 'way amk!',
+        'time': Date.now()
+    })
+})
 app.get('/about', (req, res) => res.send("<html><body><h1>this is about</body></html>"))
 app.get('/weather', (req, res) => {
     
@@ -16,7 +27,7 @@ app.get('/weather', (req, res) => {
     })  
      
 })
-app.get('*', (req, res) => res.send(`bir siktir git ${req.path}`))
+app.get('*', (req, res) => res.send(`bir siktir git lan olmayan pathler isteme: ${req.path}`))
 
 
 app.listen(3000, () => console.log("http server started on port 3000"))
